140627faca522135c0c70174bb73d1c3e581df91
Sokdenumeros <sergi.curto@gmail.com>
2023-01-05 04:28:40 +0100

    Added the car

--- Assets/Scripts/TapScript.cs
+++ Assets/Scripts/TapScript.cs
@@ -9,37 +9,43 @@ public class TapScript : MonoBehaviour
 {
     public GameObject objectToCreate;
     public Camera arCamera;
-    private GameObject newObject;
-    private ARRaycastManager RCmngr;
-    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private GameObject car;
+    //private ARRaycastManager RCmngr;
+    //static List<ARRaycastHit> hits = new List<ARRaycastHit>();
     // Start is called before the first frame update
     private void Awake()
     {
-        RCmngr = GetComponent<ARRaycastManager>();
+        //RCmngr = GetComponent<ARRaycastManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < Input.touchCount; ++i)
+        //by multiplying i by Input.touchCount or not we can make the loop only process the first touch or all of them.
+        for (int i = 0; i*Input.touchCount < Input.touchCount; ++i)
         {
             Touch t = Input.GetTouch(i);
-            if (t.phase == TouchPhase.Began)
+            if (t.phase == TouchPhase.Began || true)
             {
                 Ray ray = arCamera.ScreenPointToRay(t.position);
                 RaycastHit hitobject;
-                if (Physics.Raycast(ray, out hitobject)) physicsHitAction(hitobject.transform.gameObject);
-                else if (RCmngr.Raycast(t.position, hits, TrackableType.PlaneWithinPolygon)) arHitAction();
+                if (Physics.Raycast(ray, out hitobject)) physicsHitAction(hitobject);
+                //Physics raycast seems to interact with AR planes, but in case it was needed, here is how to do it with an ARraycast
+                //if (RCmngr.Raycast(t.position, hits, TrackableType.PlaneWithinPolygon)) arHitAction();
             }
         }
     }
 
-    private void physicsHitAction(GameObject o) {
-        Destroy(o,2);
+    private void physicsHitAction(RaycastHit o) {
+        //o.transform.gameObject;
+        if (car == null) car = Instantiate(objectToCreate, o.point, Quaternion.Euler(0,0,0));
+        else car.SendMessage("setTarget", o.point + new Vector3(0,0.2f,0), SendMessageOptions.DontRequireReceiver);
     }
 
+    /*
     private void arHitAction()
     {
-        Instantiate(objectToCreate, hits[0].pose.position, hits[0].pose.rotation);
-    }
+        if(car == null) car = Instantiate(objectToCreate, hits[0].pose.position, hits[0].pose.rotation);
+        else car.SendMessage("setTarget", hits[0].pose.position, SendMessageOptions.DontRequireReceiver);
+    }*/
 }
--- Assets/Scripts/sphereScript.cs
+++ Assets/Scripts/sphereScript.cs
@@ -2,20 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class sphereScript : MonoBehaviour
 {
+    private Rigidbody rb;
+    public float speed;
+    private Vector3 direction;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        direction = new Vector3(1, 0, 0);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void setTarget(Vector3 t) {
         
+        Vector3 move = t - transform.position;
+        move.y = 0;
+        rb.AddForce(move.normalized * speed); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.frameCount%100 > 50)
-            transform.position += Vector3.up * Time.deltaTime;
-        else
-            transform.position -= Vector3.up * Time.deltaTime;
+        if (rb.velocity.magnitude > 0.01) direction = rb.velocity;
+        //due to AR innacuracies, some frames the car moves up and down because of the plane moving, this makes the car face up or down instead of being parallell to the plane so we set y to 0
+        direction.y = 0;
+        transform.LookAt(transform.position + direction);
     }
 }
