cc81c16d55de238cf7c633fe5a41c343cfffc817
Sokdenumeros <sergi.curto@gmail.com>
2023-01-08 14:12:52 +0100

    Coin animation

--- Assets/Scripts/coinScript.cs
+++ Assets/Scripts/coinScript.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class coinScript : MonoBehaviour
 {
     public float rotationSpeed;
-
+    private int mode;
+    private float time;
     void Awake()
     {
+        time = 0.0f;
+        mode = 0;
         transform.rotation = Quaternion.Euler(90, 0,0);
     }
 
@@ -15,9 +18,18 @@ public class coinScript : MonoBehaviour
     void Update()
     {
         transform.RotateAround(transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+        if (mode == 1) {
+            time += Time.deltaTime;
+            transform.RotateAround(transform.position, Vector3.up, rotationSpeed * Time.deltaTime * 100 * (0.8f-time));
+            transform.position += new Vector3(0, 0.001f, 0)*10* (0.8f - time);
+        }
     }
 
     void carCollision() {
-        Destroy(gameObject);
+        mode = 1;
+        Destroy(gameObject,0.8f);
+        Collider col = GetComponent<Collider>();
+        col.isTrigger = true;
+        time += Time.deltaTime;
     }
 }
