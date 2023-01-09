fafaf7777c6b5f91e4e9be540869cec5f01e34ed
Adrian <adr.gruber@gmail.com>
2023-01-09 11:19:47 +0100

    added score

--- Assets/Scripts/TapScript.cs
+++ Assets/Scripts/TapScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(ARRaycastManager))]
 public class TapScript : MonoBehaviour
@@ -12,6 +13,8 @@ public class TapScript : MonoBehaviour
     public Camera arCamera;
     private GameObject car;
     private int mode;
+    public static int score;
+    Text scoreText;
     //private ARRaycastManager RCmngr;
     //static List<ARRaycastHit> hits = new List<ARRaycastHit>();
     // Start is called before the first frame update
@@ -20,17 +23,23 @@ public class TapScript : MonoBehaviour
         mode = 0;
         //RCmngr = GetComponent<ARRaycastManager>();
     }
+    void Start()
+    {
+        score = 0;
+        scoreText = GameObject.Find("Score").GetComponent<Text>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         //by multiplying i by Input.touchCount or not we can make the loop only process the first touch or all of them.
-        for (int i = 0; i*Input.touchCount < Input.touchCount; ++i)
+        for (int i = 0; i * Input.touchCount < Input.touchCount; ++i)
         {
             Touch t = Input.GetTouch(i);
             if (mode == 0 && t.phase == TouchPhase.Began && t.position.x < 300 && t.position.y < 300) mode = -1;
-            if (mode == -1) {
-                if(t.phase == TouchPhase.Ended) mode = 1;
+            if (mode == -1)
+            {
+                if (t.phase == TouchPhase.Ended) mode = 1;
                 return;
             }
             Ray ray = arCamera.ScreenPointToRay(t.position);
@@ -39,18 +48,21 @@ public class TapScript : MonoBehaviour
             if (Physics.Raycast(ray, out hitobject))
             {
                 if (mode == 0) carInteraction(hitobject);
-                else if (mode == 1 && t.phase == TouchPhase.Ended) {
+                else if (mode == 1 && t.phase == TouchPhase.Ended)
+                {
                     Instantiate(coin, hitobject.point + new Vector3(0, 0.1f, 0), Quaternion.Euler(0, 0, 0));
                     mode = 0;
                 }
             }
         }
+        scoreText.text = "Score:	 " + score;
     }
 
-    private void carInteraction(RaycastHit o) {
+    private void carInteraction(RaycastHit o)
+    {
         //o.transform.gameObject;
-        if (car == null) car = Instantiate(objectToCreate, o.point, Quaternion.Euler(0,0,0));
-        else car.SendMessage("setTarget", o.point + new Vector3(0,0.2f,0), SendMessageOptions.DontRequireReceiver);
+        if (car == null) car = Instantiate(objectToCreate, o.point, Quaternion.Euler(0, 0, 0));
+        else car.SendMessage("setTarget", o.point + new Vector3(0, 0.2f, 0), SendMessageOptions.DontRequireReceiver);
     }
 
     /*
--- Assets/Scripts/coinScript.cs
+++ Assets/Scripts/coinScript.cs
@@ -1,35 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class coinScript : MonoBehaviour
 {
     public float rotationSpeed;
     private int mode;
     private float time;
+
     void Awake()
     {
         time = 0.0f;
         mode = 0;
-        transform.rotation = Quaternion.Euler(90, 0,0);
+        transform.rotation = Quaternion.Euler(90, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.RotateAround(transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
-        if (mode == 1) {
+        if (mode == 1)
+        {
             time += Time.deltaTime;
-            transform.RotateAround(transform.position, Vector3.up, rotationSpeed * Time.deltaTime * 100 * (0.8f-time));
-            transform.position += new Vector3(0, 0.02f, 0)* (0.8f - time);
+            transform.RotateAround(transform.position, Vector3.up, rotationSpeed * Time.deltaTime * 100 * (0.8f - time));
+            transform.position += new Vector3(0, 0.02f, 0) * (0.8f - time);
         }
     }
 
-    void carCollision() {
+    void carCollision()
+    {
         mode = 1;
-        Destroy(gameObject,0.8f);
-        Collider col = GetComponent<Collider>();
-        col.isTrigger = true;
+        Destroy(gameObject, 0.8f);
+        GetComponent<Collider>().enabled = false;
         time += Time.deltaTime;
+        TapScript.score++;
     }
 }
